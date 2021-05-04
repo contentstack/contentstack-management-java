5efa3bbc0216d7492565ecc8b42b347e0757faab
shaileshmishra <Yes@383840>
2021-05-04 18:10:23 +0530

    :bulb: README and documentation added

--- src/main/java/com/contentstack/cms/core/Util.java
+++ src/main/java/com/contentstack/cms/core/Util.java
@@ -1,7 +1,15 @@
 package com.contentstack.cms.core;
 
+/**
+ * The type Util.
+ */
 public class Util {
 
+    /**
+     * Default user agent string.
+     *
+     * @return the string
+     */
     protected static String defaultUserAgent() {
         String agent = System.getProperty("http.agent");
         String operatingSystem = System.getProperty("os.name").toUpperCase();
