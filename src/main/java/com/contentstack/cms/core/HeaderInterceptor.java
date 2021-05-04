eb18cfe3d98aca05c14bc31f534a5b3f8aae555f
shaileshmishra <Yes@383840>
2021-05-04 18:10:23 +0530

    :bulb: README and documentation added

--- src/main/java/com/contentstack/cms/core/HeaderInterceptor.java
+++ src/main/java/com/contentstack/cms/core/HeaderInterceptor.java
@@ -9,10 +9,21 @@ import java.io.IOException;
 
 import static com.contentstack.cms.core.Constants.*;
 
+/**
+ * The type Header interceptor.
+ */
 public class HeaderInterceptor implements Interceptor {
 
+    /**
+     * The Authtoken.
+     */
     String authtoken;
 
+    /**
+     * Instantiates a new Header interceptor.
+     *
+     * @param authtoken the authtoken
+     */
     public HeaderInterceptor(String authtoken) {
         this.authtoken = authtoken;
     }
