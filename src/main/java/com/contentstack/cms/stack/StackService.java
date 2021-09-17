5bc16564e62febc9ddcf09899a68b1017864753a
shaileshmishra <Yes@383840>
2021-09-18 00:33:45 +0530

    :sparkles: Stack implemented

--- src/main/java/com/contentstack/cms/stack/StackService.java
+++ src/main/java/com/contentstack/cms/stack/StackService.java
@@ -1,5 +1,6 @@
 package com.contentstack.cms.stack;
 
+import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.*;
@@ -8,48 +9,33 @@ import java.util.Map;
 
 public interface StackService {
 
-    @GET("organizations")
-    Call<ResponseBody> get(@Header("authtoken") String authtoken,
-                           @QueryMap Map<String, String> queryParams);
+    @GET("stacks")
+    Call<ResponseBody> singleStack(
+            @Header("api_key") String apiKey);
 
+    @GET("stacks")
+    Call<ResponseBody> singleStack(
+            @Header("api_key") String apiKey,
+            @Header("organization_uid") String organizationUid);
 
-    @GET("organizations/{organization_uid}/roles")
-    Call<ResponseBody> getRoles(@Header("authtoken") String authtoken,
-                                @Path("organization_uid") String _uid,
-                                @QueryMap Map<String, String> queryParams);
+    @GET("stacks")
+    Call<ResponseBody> singleStack(
+            @Header("api_key") String apiKey,
+            @Header("organization_uid") String organizationUid,
+            @QueryMap Map<String, Boolean> queryParams);
 
-    @POST("organizations/{organization_uid}/share")
-    Call<ResponseBody> inviteUser(@Header("authtoken") String authtoken,
-                                  @Path("organization_uid") String _uid);
+    @POST("stacks")
+    Call<ResponseBody> createStack(
+            @Header("organization_uid") String organizationUid,
+            @Body RequestBody body);
 
+    @PUT("stacks")
+    Call<ResponseBody> updateStack(
+            @Header("api_key") String apiKey,
+            @Body RequestBody body);
 
-    @DELETE("organizations/{organization_uid}/share")
-    Call<ResponseBody> removeUser(@Header("authtoken") String authtoken,
-                                  @Path("organization_uid") String _uid);
-
-    @GET("organizations/{organization_uid}/share/{share_uid}")
-    Call<ResponseBody> resendInvitation(@Path("organization_uid") String _uid,
-                                        @Path("share_uid") String share_uid);
-
-    @GET("organizations/{organization_uid}/share")
-    Call<ResponseBody> getAllInvitations(@Header("authtoken") String authtoken,
-                                         @Path("organization_uid") String _uid,
-                                         @QueryMap Map<String, String> queryParams);
-
-    @FormUrlEncoded
-    @POST("organizations/{organization_uid}/ownership")
-    Call<ResponseBody> transferOwnership(@Header("authtoken") String authtoken,
-                                         @Path("organization_uid") String _uid, @Body String transfer_to);
-
-    @GET("organizations/{organization_uid}/stacks")
-    Call<ResponseBody> getStacks(@Header("authtoken") String authtoken,
-                                 @Path("organization_uid") String _uid,
-                                 @QueryMap Map<String, String> queryParams);
-
-
-    @GET("organizations/{organization_uid}/logs/{log_uid}")
-    Call<ResponseBody> getLogs(@Header("authtoken") String authtoken,
-                               @Path("organization_uid") String _uid,
-                               @Path("log_uid") String log_uid);
-
+    @POST("stacks/transfer_ownership")
+    Call<ResponseBody> transferOwnership(
+            @Header("api_key") String apiKey,
+            @Body RequestBody body);
 }
