62f620cd5b1890db78f151ed70cab3ab78b802b8
Shailesh Mishra <mshaileshr@gmail.com>
2022-05-04 12:28:25 +0530

    :potted_plant: Label class added

--- src/main/java/com/contentstack/cms/core/CMSRuntimeException.java
+++ src/main/java/com/contentstack/cms/core/CMSRuntimeException.java
@@ -1,7 +1,6 @@
 package com.contentstack.cms.core;
 
 public class CMSRuntimeException extends Exception {
-
     public CMSRuntimeException(String message) {
         super(message);
     }
--- src/main/java/com/contentstack/cms/stack/Stack.java
+++ src/main/java/com/contentstack/cms/stack/Stack.java
@@ -103,10 +103,11 @@ public class Stack {
     }
 
     /**
-     * An entry is the actual piece of content created using one of the defined [content
-     * types](https://www.contentstack.com/docs/developers/create-content-types/about-content-types/).
+     * An entry is the actual piece of content created using one of the defined
      *
      * @return {@link Entry} Entry instance
+     * @see <a href="https://www.contentstack.com/docs/developers/create-content-types/about-content-types">Content
+     * Type</a>
      */
     public Entry entry(@NotNull String contentTypeUid) {
         return new Entry(this.client, this.headers, contentTypeUid);
@@ -125,9 +126,9 @@ public class Stack {
      * language. This feature allows you to set up multilingual websites and cater to a wide variety of audience by
      * serving content in their local language(s).
      * <p>
-     * Read more about [Languages](https://www.contentstack.com/docs/developers/multilingual-content).
      *
      * @return Locale
+     * @see <a href="https://www.contentstack.com/docs/developers/multilingual-content">Languages</a>
      */
     public Locale locale() {
         return new Locale(client, headers);
@@ -138,12 +139,33 @@ public class Stack {
      * A publishing environment corresponds to one or more deployment servers or a content delivery destination where
      * the entries need to be published.
      * <p>
-     * Read more about [Environments](https://www.contentstack.com/docs/developers/set-up-environments).
+     * Read more about
      *
      * @return Environment
+     * @see <a href="https://www.contentstack.com/docs/developers/set-up-environments">Environments</a>
      */
     public Environment environment() {
         return new Environment(client, this.headers);
+    }
+
+
+    /**
+     * Labels allow you to group a collection of content within a stack. Using labels you can group content types that
+     * need to work together. Read more about
+     *
+     * @return @{@link Label}
+     * <p>
+     * @see <a href="https://www.contentstack.com/docs/developers/create-content-types/manage-labels">Labels</a>
+     *
+     * <p>
+     * You can now pass the branch header in the API request to fetch or manage modules located within specific branches
+     * of the stack. Additionally, you can also set the include_branch query parameter to true to include the _branch
+     * top-level key in the response. This key specifies the unique ID of the branch where the concerned Contentstack
+     * module resides.
+     * <p>
+     */
+    public Label label() {
+        return new Label(client, this.headers);
     }
 
     /**
@@ -169,7 +191,7 @@ public class Stack {
      * it is mandatory to pass the organization UID in the header.
      *
      * @param organizationUid
-     *                        the organization uid
+     *         the organization uid
      * @return the stack
      */
     public Call<ResponseBody> fetch(@NotNull String organizationUid) {
@@ -185,9 +207,9 @@ public class Stack {
      * it is mandatory to pass the organization UID in the header.
      *
      * @param orgUID
-     *               the organization uid
+     *         the organization uid
      * @param query
-     *               the query param
+     *         the query param
      * @return the stack
      */
     public Call<ResponseBody> fetch(@NotNull String orgUID,
@@ -207,8 +229,9 @@ public class Stack {
      * stack per minute.
      *
      * @param organizationUid
-     *                        the organization uid
-     * @param requestBody The request body as JSONObject
+     *         the organization uid
+     * @param requestBody
+     *         The request body as JSONObject
      * @return the stack
      */
     public Call<ResponseBody> create(
@@ -219,18 +242,15 @@ public class Stack {
     /**
      * <b>Update Stack</b>
      * <br>
-     * The Update stack call lets you update the name and description of an existing
-     * stack.
+     * The Update stack call lets you update the name and description of an existing stack.
      * <br>
-     * In the 'Body' section, provide the updated schema of the stack in JSON
-     * format.
+     * In the 'Body' section, provide the updated schema of the stack in JSON format.
      * <br>
      * <b>Note</b> Warning: The master locale cannot be changed once it is set
-     * while stack creation. So, you cannot use this call to change/update the
-     * master language.
+     * while stack creation. So, you cannot use this call to change/update the master language.
      *
      * @param requestBody
-     *                   the Request body
+     *         the Request body
      * @return the stack
      */
     public Call<ResponseBody> update(@NotNull JSONObject requestBody) {
@@ -240,32 +260,28 @@ public class Stack {
     /**
      * <b>Transfer Stack Ownership</b>
      * <br>
-     * The Transfer stack ownership to other users call sends the specified user an
-     * email invitation for accepting the
+     * The Transfer stack ownership to other users call sends the specified user an email invitation for accepting the
      * ownership of a particular stack.
      *
      * <br>
      * <br>
-     * Once the specified user accepts the invitation by clicking on the link
-     * provided in the email, the ownership of
-     * the stack gets transferred to the new user. Subsequently, the previous owner
-     * will no longer have any permission
+     * Once the specified user accepts the invitation by clicking on the link provided in the email, the ownership of
+     * the stack gets transferred to the new user. Subsequently, the previous owner will no longer have any permission
      * on the stack.
      * <br>
      * <b>Note</b>
      * <br>
      * <b>
-     * Warning: The master locale cannot be changed once it is set while stack
-     * creation. So, you cannot use this call to
+     * Warning: The master locale cannot be changed once it is set while stack creation. So, you cannot use this call to
      * change/update the master language.
      * </b>
-     *
+     * <p>
      * <a href=
      * "https://www.contentstack.com/docs/developers/apis/content-management-api/#transfer-stack-ownership-to-other-users">
      * (Read more) </a>
      *
      * @param requestBody
-     *                    The request body as JSONObject
+     *         The request body as JSONObject
      * @return the stack
      */
     public Call<ResponseBody> transferOwnership(@NotNull JSONObject requestBody) {
@@ -275,25 +291,22 @@ public class Stack {
     /**
      * <b>Accept Stack Ownership</b>
      * <br>
-     * The Accept stack owned by other user call allows a user to accept the
-     * ownership of a particular stack via an
+     * The Accept stack owned by other user call allows a user to accept the ownership of a particular stack via an
      * email invitation.
      * <br>
      * <p>
-     * Once the user accepts the invitation by clicking on the link, the ownership
-     * is transferred to the new user
-     * account. Subsequently, the user who transferred the stack will no longer have
-     * any permission on the stack.
+     * Once the user accepts the invitation by clicking on the link, the ownership is transferred to the new user
+     * account. Subsequently, the user who transferred the stack will no longer have any permission on the stack.
      * <br>
-     *
+     * <p>
      * <a href=
      * "https://www.contentstack.com/docs/developers/apis/content-management-api/#transfer-stack-ownership-to-other-users">
      * (Read more) </a>
      *
      * @param ownershipToken
-     *                       the ownership token received via email by another user.
+     *         the ownership token received via email by another user.
      * @param uid
-     *                       the user uid.
+     *         the user uid.
      * @return the stack
      */
     public Call<ResponseBody> acceptOwnership(
@@ -311,9 +324,8 @@ public class Stack {
      * configuration settings of an existing stack.</b>
      * <br>
      *
-     * <a href=
-     * "https://www.contentstack.com/docs/developers/apis/content-management-api/#stack-settings">
-     * *(Read more)</a>
+     * <a href= "https://www.contentstack.com/docs/developers/apis/content-management-api/#stack-settings"> *(Read
+     * more)</a>
      *
      * @return the call
      */
@@ -324,23 +336,19 @@ public class Stack {
     /**
      * <b>Add/Update Stack Settings</b>
      * <p>
-     * The Add stack settings request lets you add additional settings for your
-     * existing stack.
+     * The Add stack settings request lets you add additional settings for your existing stack.
      * </p>
      * <br>
      * <p>
-     * You can add specific settings for your stack by passing any of the following
-     * parameters within the
+     * You can add specific settings for your stack by passing any of the following parameters within the
      * stack_variables section in the <b>Request Body</b>:
      * </p>
      * <p>
      * Additionally, you can pass <b>cs_only_break line</b>: true under the
      * <b>rte</b> parameter to ensure that only a
      * <br>
-     * tag is added inside the <b>Rich Text Editor</b> field when the content
-     * manager presses <b>Enter</b>. When
-     * this parameter is set to false, the <br>
-     * tag is replaced with
+     * tag is added inside the <b>Rich Text Editor</b> field when the content manager presses <b>Enter</b>. When this
+     * parameter is set to false, the <br> tag is replaced with
      * <p>
      * <br>
      * </p>
@@ -360,20 +368,18 @@ public class Stack {
      *  }
      * </pre>
      *
-     * <a href=
-     * "https://www.contentstack.com/docs/developers/apis/content-management-api/#add-stack-settings">
-     * *(Read more)</a>
+     * <a href= "https://www.contentstack.com/docs/developers/apis/content-management-api/#add-stack-settings"> *(Read
+     * more)</a>
      * <br>
      * <br>
      * <b>Reset stack settings</b>
      * <p>
-     * The Reset stack settings call resets your stack to default settings, and
-     * additionally, lets you add
-     * parameters to or modify the settings of an existing stack.
+     * The Reset stack settings call resets your stack to default settings, and additionally, lets you add parameters to
+     * or modify the settings of an existing stack.
      * </p>
      * <br>
      * <b>Here is a sample of the Request Body:</b>
-     * 
+     *
      * <pre>
      *
      * {
@@ -384,12 +390,11 @@ public class Stack {
      * }
      * </pre>
      *
-     * <a href=
-     * "https://www.contentstack.com/docs/developers/apis/content-management-api/#reset-stack-settings">
-     * (Read more)</a>
+     * <a href= "https://www.contentstack.com/docs/developers/apis/content-management-api/#reset-stack-settings"> (Read
+     * more)</a>
      *
      * @param requestBody
-     *                    the request body as JSONObject
+     *         the request body as JSONObject
      * @return the call
      */
     public Call<ResponseBody> updateSetting(@NotNull JSONObject requestBody) {
@@ -399,13 +404,12 @@ public class Stack {
     /**
      * <b>Reset stack settings</b>
      * <p>
-     * The Reset stack settings call resets your stack to default settings, and
-     * additionally, lets you add
-     * parameters to or modify the settings of an existing stack.
+     * The Reset stack settings call resets your stack to default settings, and additionally, lets you add parameters to
+     * or modify the settings of an existing stack.
      * </p>
      * <br>
      * <b>Here is a sample of the Request Body:</b>
-     * 
+     *
      * <pre>
      *
      * {
@@ -416,12 +420,11 @@ public class Stack {
      * }
      * </pre>
      *
-     * <a href=
-     * "https://www.contentstack.com/docs/developers/apis/content-management-api/#reset-stack-settings">
-     * (Read more)</a>
+     * <a href= "https://www.contentstack.com/docs/developers/apis/content-management-api/#reset-stack-settings"> (Read
+     * more)</a>
      *
      * @param requestBody
-     *                    the request body
+     *         the request body
      * @return the call
      */
     public Call<ResponseBody> resetSetting(@NotNull JSONObject requestBody) {
@@ -431,17 +434,15 @@ public class Stack {
     /**
      * <b>Share a stack</b>
      * <p>
-     * The Share a stack call shares a stack with the specified user to collaborate
-     * on the stack.
+     * The Share a stack call shares a stack with the specified user to collaborate on the stack.
      * </p>
      * <br>
      * <p>
-     * In the 'Body' section, you need to provide the email ID of the user with whom
-     * you wish to share the stack along
+     * In the 'Body' section, you need to provide the email ID of the user with whom you wish to share the stack along
      * with the role uid that you wish to assign the user.
      * </p>
      * <b>Here is a sample of the Request Body:</b>
-     * 
+     *
      * <pre>
      *    {
      * 	"emails": [
@@ -456,7 +457,7 @@ public class Stack {
      * </pre>
      *
      * @param requestBody
-     *                    the request body
+     *         the request body
      * @return the call
      */
     public Call<ResponseBody> share(@NotNull JSONObject requestBody) {
@@ -466,15 +467,12 @@ public class Stack {
     /**
      * <b>Unshare a stack</b>
      * <p>
-     * The Unshare a stack removes the user
-     * account from the list of
-     * collaborators. Once this call is executed, the user will not be able to view
-     * the stack in their account.
+     * The Unshare a stack removes the user account from the list of collaborators. Once this call is executed, the user
+     * will not be able to view the stack in their account.
      * </p>
      * <br>
      * <p>
-     * In the 'Body' section, you need to provide the email ID of the user from whom
-     * you wish to unshare the stack.
+     * In the 'Body' section, you need to provide the email ID of the user from whom you wish to unshare the stack.
      * </p>
      * <b>Here is a sample of the Request Body:</b>
      *
@@ -485,7 +483,7 @@ public class Stack {
      * </pre>
      *
      * @param requestBody
-     *                    the request body
+     *         the request body
      * @return the call
      */
     public Call<ResponseBody> unshare(@NotNull JSONObject requestBody) {
@@ -496,8 +494,7 @@ public class Stack {
      * <b>Get all users of a stack</b>
      * <br>
      * <p>
-     * The Get all users of a stack call fetches
-     * the list of all users of a particular stack
+     * The Get all users of a stack call fetches the list of all users of a particular stack
      * </p>
      *
      * @return the call
@@ -510,18 +507,14 @@ public class Stack {
      * <b>Update User Role</b>
      * <br>
      * <p>
-     * The Update User Role API Request updates the roles of an existing user
-     * account. This API Request will override
-     * the existing roles assigned to a user. For example, we have an existing user
-     * with the <b>Developer</b> role, and
-     * if you execute this API request with "Content Manager" role, the user role
-     * will lose <b>Developer</b> rights and
+     * The Update User Role API Request updates the roles of an existing user account. This API Request will override
+     * the existing roles assigned to a user. For example, we have an existing user with the <b>Developer</b> role, and
+     * if you execute this API request with "Content Manager" role, the user role will lose <b>Developer</b> rights and
      * the user role be updated to just <b>Content Manager</b>.
      * </p>
      * <br>
      * <p>
-     * When executing the API call, under the <b>Body</b> section, enter the UIDs of
-     * roles that you want to assign a
+     * When executing the API call, under the <b>Body</b> section, enter the UIDs of roles that you want to assign a
      * user. This information should be in JSON format.
      * <b>Here is a sample of the Request Body:</b>
      *
@@ -534,11 +527,12 @@ public class Stack {
      * </pre>
      *
      * @param requestBody
-     *                    the request body
+     *         the request body
      * @return the {@link okhttp3.Call}
      */
     public Call<ResponseBody> roles(@NotNull JSONObject requestBody) {
         return stackService.updateUserRoles(this.headers, requestBody);
     }
+
 
 }
