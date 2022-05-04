eade29cf1fb4de34804eed3e89c4aeabb49bb8f6
Shailesh Mishra <mshaileshr@gmail.com>
2022-05-04 12:28:25 +0530

    :potted_plant: Label class added

---
+++ src/main/java/com/contentstack/cms/stack/LabelService.java
@@ -0,0 +1,48 @@
+package com.contentstack.cms.stack;
+
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import org.json.simple.JSONObject;
+import retrofit2.Call;
+import retrofit2.http.*;
+
+import java.util.Map;
+
+public interface LabelService {
+
+    @GET("labels")
+    Call<RequestBody> get(
+            @HeaderMap Map<String, Object> headers);
+
+    @GET("labels")
+    Call<RequestBody> get(
+            @HeaderMap Map<String, Object> headers,
+            @QueryMap(encoded = true) Map<String, Object> queryParameter);
+
+    @GET("labels/{label_uid}")
+    Call<ResponseBody> get(
+            @HeaderMap Map<String, Object> headers,
+            @Path("label_uid") String labelUid);
+
+    @GET("labels/{label_uid}")
+    Call<ResponseBody> get(
+            @HeaderMap Map<String, Object> headers,
+            @Path("label_uid") String labelUid,
+            @QueryMap(encoded = true) Map<String, Object> queryParameter);
+
+    @POST("labels")
+    Call<ResponseBody> add(
+            @HeaderMap Map<String, Object> headers,
+            @Body JSONObject body);
+
+    @PUT("labels/{label_uid}")
+    Call<ResponseBody> update(
+            @HeaderMap Map<String, Object> headers,
+            @Path("label_uid") String labelUid,
+            @Body JSONObject body);
+
+    @DELETE("labels/{label_uid}")
+    Call<ResponseBody> delete(
+            @HeaderMap Map<String, Object> headers,
+            @Path("label_uid") String labelUid);
+}
