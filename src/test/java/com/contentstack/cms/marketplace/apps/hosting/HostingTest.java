cb6f267c16cd5e5da9f830bcfdb7e132f5bea6cd
Shailesh Mishra <mshaileshr@gmail.com>
2023-05-17 18:47:58 +0530

    feat/[CS-38337]: Support for marketplace api

---
+++ src/test/java/com/contentstack/cms/marketplace/apps/hosting/HostingTest.java
@@ -0,0 +1,12 @@
+package com.contentstack.cms.marketplace.apps.hosting;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class HostingTest {
+
+    @Test
+    void testHosting() {
+        Assertions.assertNotNull("ioi");
+    }
+}
