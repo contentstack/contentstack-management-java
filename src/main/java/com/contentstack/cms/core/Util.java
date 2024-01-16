d53526c0f32d97ba2a07679229940c8467acbc6d
Shailesh Mishra <mshaileshr@gmail.com>
2024-01-16 11:46:53 +0530

    CS-42937 : Support for Early Access Header CS-42936 : Taxonomy document and Test

--- src/main/java/com/contentstack/cms/core/Util.java
+++ src/main/java/com/contentstack/cms/core/Util.java
@@ -15,10 +15,10 @@ import java.util.Objects;
  */
 public class Util {
 
-    // The line `public static final String SDK_VERSION = "1.0.0";`
+    // The line `public static final String SDK_VERSION = "1.2.0";`
     // named `SDK_VERSION` of type `String`. The value of this constant is set to
-    // "1.0.0".
-    public static final String SDK_VERSION = "1.0.0";
+    // "1.2.0".
+    public static final String SDK_VERSION = "1.2.0";
 
     static final String PRIVATE_CONSTRUCTOR = "private constructor can't be accessed outside the class";
     public static final Boolean RETRY_ON_FAILURE = true;
